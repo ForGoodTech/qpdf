751fbf68eddb0a55f4562399821d061ed9c88d2b
ForGoodTech <88217240+ForGoodTech@users.noreply.github.com>
2025-08-11 01:50:40 -0400

    Enable object stream and data compression in WASM shim

--- wasm/shim.cc
+++ wasm/shim.cc
@@ -1,14 +1,18 @@
+#include <qpdf/Constants.h>
 #include <qpdf/QPDF.hh>
 #include <qpdf/QPDFWriter.hh>
 #include <exception>
 
-extern "C" int qpdf_wasm_compress(char const* infilename, char const* outfilename)
+extern "C" int
+qpdf_wasm_compress(char const* infilename, char const* outfilename)
 {
     try {
         QPDF pdf;
         pdf.processFile(infilename);
         QPDFWriter w(pdf, outfilename);
-        w.setCompressStreams(true);
+        w.setObjectStreamMode(qpdf_o_generate);
+        w.setStreamDataMode(qpdf_s_compress);
+        w.setRecompressFlate(true);
         w.write();
         return 0;
     } catch (std::exception& e) {
